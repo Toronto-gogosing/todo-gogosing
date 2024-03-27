8d421965dbe718288b8fb29167e81b1863a56708
xvzc <dev.kwanghoo@gmail.com>
2024-03-27 13:51:29 -0400

    chore: change file extensions for application.properties and build.gradle

---
+++ todo/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.benchmark"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc:3.2.4")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
+    implementation( "io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation( "io.jsonwebtoken:jjwt-impl:0.12.5")
+    implementation( "io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+
+//    implementation group: , name: 'jjwt-api', version: '0.11.5'
+//    runtimeOnly group: 'io.jsonwebtoken', name: 'jjwt-impl', version: '0.11.5'
+//    runtimeOnly group: 'io.jsonwebtoken', name: 'jjwt-jackson', version: '0.11.5'
+
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
