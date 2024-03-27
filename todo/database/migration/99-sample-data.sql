e952af60bf78b87ffe6586454a0ef7ccd74b602c
xvzc <dev.kwanghoo@gmail.com>
2024-03-27 13:50:04 -0400

    chore: Update database

---
+++ todo/database/migration/99-sample-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO todo.user (`name`, `username`, `password`)
+VALUES ('Jack', 'jack100', '1234');
+INSERT INTO todo.user (`name`, `username`, `password`)
+VALUES ('John', 'john1089', '1234');
+INSERT INTO todo.user (`name`, `username`, `password`)
+VALUES ('Bradley', 'brad801', '1234');
