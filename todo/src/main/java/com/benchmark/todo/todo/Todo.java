a368eba74e179dc9346e076c9f4e495775b182e6
James Lee <js44lee@gmail.com>
2024-03-27 16:20:17 -0400

    conflict resolved before rebasing

--- todo/src/main/java/com/benchmark/todo/todo/Todo.java
+++ todo/src/main/java/com/benchmark/todo/todo/Todo.java
@@ -1,10 +1,14 @@
 package com.benchmark.todo.todo;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Todo {
 
     @Id
@@ -18,30 +22,6 @@ public class Todo {
 
     public Todo(LocalDateTime dateTime, String description) {
         this.dateTime = dateTime;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
