db9503cfc7e315042ea93b20fd9142b5cae36bb2
xvzc <dev.kwanghoo@gmail.com>
2024-03-27 13:55:18 -0400

    feat: improve user domain

---
+++ todo/src/main/java/com/benchmark/todo/user/service/UserService.java
@@ -0,0 +1,24 @@
+package com.benchmark.todo.user.service;
+
+import com.benchmark.todo._core.error.CommonException;
+import com.benchmark.todo._core.error.ErrorCode;
+import com.benchmark.todo.user.dto.UserDTO;
+import com.benchmark.todo.user.entity.User;
+import com.benchmark.todo.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserDTO.Slim fetchUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() ->
+                CommonException.of(ErrorCode.NOT_FOUND)
+        );
+
+        return UserDTO.Slim.of(user);
+    }
+}
